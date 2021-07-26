54cf63749332b4c3739559cc515b5444ec01884d
Fabio Vanzella <fabio.vanzella@tjmt.jus.br>
2021-07-26 09:39:48 -0400

    first commit

---
+++ api_rest.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 09:31:16 2021
+
+@author: fabio
+"""
+
+
+import flask
+import numpy as np
+import tensorflow as tf
+
+from flask import request, jsonify
+
+ # Disable all GPUS
+tf.config.set_visible_devices([], 'GPU')
+visible_devices = tf.config.get_visible_devices()
+for device in visible_devices:
+    assert device.device_type != 'GPU'
+        
+# Carrega o melhor modelo
+model = tf.keras.models.load_model("/home/fabio/Documentos/TCC/modelos/model.weights.best.hdf5")
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+IMG_SIZE = (224,224)
+FILE_UPLOAD = 'upload'
+
+@app.route('/predict', methods=['POST'])
+def predict():  
+    
+    file = request.files['file']
+    file.save(FILE_UPLOAD)
+        
+    img = tf.keras.preprocessing.image.img_to_array(
+            tf.keras.preprocessing.image.load_img(FILE_UPLOAD, target_size=IMG_SIZE, color_mode='rgb'))/255
+            
+    # Dimensiona a imagem
+    img = np.expand_dims(img,0)
+    
+    print(img.shape)
+    
+    # Aplica a prediçao
+    pred = model.predict(img)
+    
+    #print('Classification Report')
+    
+    resultado = jsonify(
+        normal= str(round(pred[0][0], 2)),
+        pneumonia= str(round(pred[0][1], 2))
+    )
+    return resultado
+
+app.run()
